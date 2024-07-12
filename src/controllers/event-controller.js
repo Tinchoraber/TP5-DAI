40d37181373199ee5362df3c151af583217faa8d
Android Studio <devandroid@ort>
2024-07-12 10:30:53 -0300

    .

--- src/controllers/event-controller.js
+++ src/controllers/event-controller.js
@@ -16,15 +16,29 @@ const svc = new eventService();
     });
 
     //3
-    router.get('/', async (req, res, next) => {
-      const { offset, limit, name, category, startDate, tag } = req.query;
-      const url = req.originalUrl;
-      
-      eventService
-      .searchEventsAynsc(limit, offset, url, name, category, startDate, tag)
-      .then(user => res.status(200).json(user))
-      .catch(err => next(err));
-      });
+    router.get('/', async (req, res) => {
+        let result;
+        if (req.query === undefined || Object.keys(req.query).length === 0) {
+            result = await eventService.getAllEvents();
+        }
+        else {
+            const name = getString(req.query.name);
+            const category = getString(req.query.category);
+            const startDate = getDate(req.query.start_date);
+            const tag = getString(req.query.tag);
+            result = await eventService.getFilteredEvent(name, category, startDate, tag);
+            if (!result) {
+                res.status(404).send('No se encontraron eventos que cumplan con los criterios de búsqueda');
+                return;
+            }
+        }
+        if (result) {
+            res.status(200).send(result);
+        }
+        else {
+            res.status(500).send('Error interno');
+        }
+    });
      //4)   
      router.get('/:id', async (req, res) => {
       const id = req.params.id; // Obtener el ID del parámetro de la URL
